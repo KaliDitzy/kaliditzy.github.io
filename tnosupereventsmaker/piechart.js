76efb5917bd3e3a0965dd00038d14f15ea3789c6
KaliDitzy <kalisightler3@gmail.com>
2024-07-23 09:37:04 -0500

    style updates + Better support for mobile devices

--- tnosupereventsmaker/piechart.js
+++ tnosupereventsmaker/piechart.js
@@ -1,3 +1,22 @@
+const colorShade = (col, amt) => {
+  col = col.replace(/^#/, '')
+  if (col.length === 3) col = col[0] + col[0] + col[1] + col[1] + col[2] + col[2]
+
+  let [r, g, b] = col.match(/.{2}/g);
+  ([r, g, b] = [parseInt(r, 16) + amt, parseInt(g, 16) + amt, parseInt(b, 16) + amt])
+
+  r = Math.max(Math.min(255, r), 0).toString(16)
+  g = Math.max(Math.min(255, g), 0).toString(16)
+  b = Math.max(Math.min(255, b), 0).toString(16)
+
+  const rr = (r.length < 2 ? '0' : '') + r
+  const gg = (g.length < 2 ? '0' : '') + g
+  const bb = (b.length < 2 ? '0' : '') + b
+
+  return `#${rr}${gg}${bb}`
+}
+
+
 function drawLine(ctx, startX, startY, endX, endY, color) {
   ctx.save();
   ctx.strokeStyle = color;
@@ -41,27 +60,43 @@ function drawPieSlice(
 
 function drawPieChart(party1, party2, party3, party4, party1color, party2color, party3color, party4color) {
   var pieChart = document.getElementById("pieChart");
-  pieChart.width = 512;
-  pieChart.height = 512;
+  pieChart.width = 68;
+  pieChart.height = 68;
   var ctx = pieChart.getContext("2d");
 
-  var x = 175
-  var y = 305
+  var x = 34
+  var y = 34
   var radius = 34
   var twoRad = Math.PI * 2
 
+  if (party1 <= 0) { return }
   var beginAngle = 0
   var endAngle = twoRad * party1 / 100
   drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party1color, party1color)
+  for (let i = 0; i < 4; i++) {
+		drawArc(ctx, x, y, radius-(i*0.75), beginAngle, endAngle, colorShade(party1color, -96));
+  }
+  if (party2 <= 0) { return }
   var beginAngle = endAngle
   var endAngle = beginAngle + (twoRad * party2 / 100)
   drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party2color, party2color)
+  for (let i = 0; i < 4; i++) {
+		drawArc(ctx, x, y, radius-(i*0.75), beginAngle, endAngle, colorShade(party2color, -96));
+  }
+  if (party3 <= 0) { return }
   var beginAngle = endAngle
   var endAngle = beginAngle + (twoRad * party3 / 100)
   drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party3color, party3color)
+  for (let i = 0; i < 4; i++) {
+		drawArc(ctx, x, y, radius-(i*0.75), beginAngle, endAngle, colorShade(party3color, -96));
+  }
+  if (party4 <= 0) { return }
   var beginAngle = endAngle
   var endAngle = beginAngle + (twoRad * party4 / 100)
   drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party4color, party4color)
+  for (let i = 0; i < 4; i++) {
+		drawArc(ctx, x, y, radius-(i*0.75), beginAngle, endAngle, colorShade(party4color, -96));
+  }
 }
 
 function UpdateChart() {
