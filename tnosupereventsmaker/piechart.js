818485fa68c699683519235927915597eb982653
KaliDitzy <kalisightler3@gmail.com>
2024-07-24 04:11:31 -0500

    INFINITE PARTIES

--- tnosupereventsmaker/piechart.js
+++ tnosupereventsmaker/piechart.js
@@ -58,7 +58,7 @@ function drawPieSlice(
   ctx.restore();
 }
 
-function drawPieChart(party1, party2, party3, party4, party1color, party2color, party3color, party4color) {
+function drawPieChart(parties, partycolors) {
   var pieChart = document.getElementById("pieChart");
   pieChart.width = 68;
   pieChart.height = 68;
@@ -68,35 +68,22 @@ function drawPieChart(party1, party2, party3, party4, party1color, party2color, 
   var y = 34
   var radius = 34
   var twoRad = Math.PI * 2
-
-  if (party1 <= 0) { return }
+  
+  parties = parties.split(",")
+  partycolors = partycolors.split(",")
   var beginAngle = 0
-  var endAngle = twoRad * party1 / 100
-  drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party1color, party1color)
-  if (party2 <= 0) { return }
-  var beginAngle = endAngle
-  var endAngle = beginAngle + (twoRad * party2 / 100)
-  drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party2color, party2color)
-  if (party3 <= 0) { return }
-  var beginAngle = endAngle
-  var endAngle = beginAngle + (twoRad * party3 / 100)
-  drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party3color, party3color)
-  if (party4 <= 0) { return }
-  var beginAngle = endAngle
-  var endAngle = beginAngle + (twoRad * party4 / 100)
-  drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, party4color, party4color)
+  var endAngle = 0
+	for (let i = 0; i < parties.length; i++) {
+		endAngle += twoRad * parties[i] / 100
+		drawPieSlice(ctx, x, y, radius, beginAngle, endAngle, partycolors[i], partycolors[i])
+		beginAngle = endAngle
+	}
 }
 
 function UpdateChart() {
-  var party1 = document.getElementById("party1").value
-  var party1color = document.getElementById("party1color").value
-  var party2 = document.getElementById("party2").value
-  var party2color = document.getElementById("party2color").value
-  var party3 = document.getElementById("party3").value
-  var party3color = document.getElementById("party3color").value
-  var party4 = document.getElementById("party4").value
-  var party4color = document.getElementById("party4color").value
-  drawPieChart(party1, party2, party3, party4, party1color, party2color, party3color, party4color)
+  var parties = document.getElementById("parties").value
+  var partycolors = document.getElementById("partycolors").value
+  drawPieChart(parties,partycolors)
 }
 
 //drawPieChart(75, 0, 25, 0, "#F00", "#FFF", "#0F0", "#00F")
